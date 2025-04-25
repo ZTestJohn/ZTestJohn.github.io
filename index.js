3f65380e69b067cae8cee74642a15bb982636a1b
João Vitor <ZTestJohn@users.noreply.github.com>
2025-04-25 18:15:58 -0300

    +

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import getGithubToken from "./github_token.js";
+
+const { github_token } = getGithubToken();
+
 const texts = [
   "Eu realmente gosto de programar",
   "React é uma ótima invenção humana",
@@ -41,3 +45,57 @@ let isDeleting = false;
     setTimeout(type, speed);
   }
 })();
+
+async function fetchRepositories() {
+  const username = "ZTestJohn";
+  const projectsContainer = document.querySelector(".projects");
+  const token = github_token; 
+
+  try {
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${username}/repos`,
+      {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      }
+    );
+
+    if (!reposResponse.ok) {
+      throw new Error(`Erro na API: ${reposResponse.status}`);
+    }
+
+    const repos = await reposResponse.json();
+
+    for (const repo of repos) {
+      const languagesResponse = await fetch(repo.languages_url);
+      const languages = await languagesResponse.json();
+
+      const totalBytes = Object.values(languages).reduce((a, b) => a + b, 0);
+      const mainLanguage = Object.keys(languages)[0] || "N/A";
+      const mainLanguagePercentage = totalBytes
+        ? ((languages[mainLanguage] / totalBytes) * 100).toFixed(2)
+        : "0";
+
+      const card = document.createElement("div");
+      card.classList.add("card-project");
+
+      card.innerHTML = `
+        <a class="repository-name" href="${repo.html_url}" target="_blank">${
+        repo.name
+      }</a>
+        <p class="project-description">${
+          repo.description || "Sem descrição disponível"
+        }</p>
+        <p class="project-language">${mainLanguage} - ${mainLanguagePercentage}%</p>
+      `;
+      projectsContainer.appendChild(card);
+    }
+  } catch (error) {
+    console.error("Erro ao buscar repositórios:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchRepositories();
+});
